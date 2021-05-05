d5cc025ac945cb510c9822931df248b994198739
Odette Quiroz <odette.qmon@gmail.com>
2021-05-04 20:12:15 -0500

    linux platform added, bundler install error fixed

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   root to: 'lists#index'
 
   resources :lists, only: [:show, :new, :create] do
-    resources :bookmarks, only: [:new, :create, :delete]
+    resources :bookmarks, only: [:new, :create]
   end
+  resources :bookmarks, only: [:destroy]
 end
