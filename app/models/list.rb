2f0dbc3ac50502094c7e86e8283e6f8593d47604
Odette Quiroz <odette.qmon@gmail.com>
2021-05-05 19:02:51 -0500

    style and delete options

--- app/models/list.rb
+++ app/models/list.rb
@@ -2,4 +2,6 @@ class List < ApplicationRecord
   has_many :bookmarks, dependent: :destroy
   has_many :movies, through: :bookmarks
   validates :name, presence: true, uniqueness: true
+  # cloudinary and active storage config
+  has_one_attached :photo
 end
